279ffd27d30d3579061c3d689a30dfa844b70aeb
Leander Grech <leander.grech@cern.ch>
2019-11-27 13:25:54 +0100

    Added args to ppo.py

--- src/ppo.py
+++ src/ppo.py
@@ -4,11 +4,19 @@ from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import PPO2
 
-env = gym.make('simpleEnv:simpleEnv-v0')
-env = DummyVecEnv([lambda: env])
+import argparse
 
-model = PPO2(MlpPolicy, env, verbose=1)
-model.learn(total_timesteps=5000)
 
-model.save('simpleEnv-full5x5')
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('total_timesteps')
+	args = parser.parse_args()
+
+	env = gym.make('simpleEnv:simpleEnv-v0')
+	env = DummyVecEnv([lambda: env])
+
+	model = PPO2(MlpPolicy, env, verbose=1)
+	model.learn(total_timesteps=int(args.total_timesteps))
+
+	model.save('simpleEnv-full5x5')
 
