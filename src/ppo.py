1357de9ed405ac46a958748ff73ce7bca44ece5b
Leander Grech <lgrech@tlab-gpu-orbitfeedback.cern.ch>
2019-11-29 17:48:59 +0100

    Small changed to ppo.py on handling KeyboardInterrupt Added training_log.md

--- src/ppo.py
+++ src/ppo.py
@@ -8,15 +8,26 @@ import argparse
 
 
 if __name__ == '__main__':
+	model_name = 'simpleEnv-full550x550'	
+
 	parser = argparse.ArgumentParser()
 	parser.add_argument('total_timesteps')
+	parser.add_argument('tb_log')
 	args = parser.parse_args()
 
 	env = gym.make('simpleEnv:simpleEnv-v0')
+	print('\n\n')
+	print(f"Starting training on env having shape = {env.response_matrix.shape}")
+	print('\n\n')
+	sleep(4)
+
 	env = DummyVecEnv([lambda: env])
+	model = PPO2(MlpPolicy, env, tensorboard_log=args.tb_log, verbose=1)
 
-	model = PPO2(MlpPolicy, env, tensorboard_log='log', verbose=1)
-	model.learn(total_timesteps=int(args.total_timesteps))
+	try:
+		model.learn(total_timesteps=int(args.total_timesteps))
+	except KeyboardInterrupt:
+		pass
 
-	model.save('simpleEnv-full5x5')
+	model.save(model_name)
 
---
+++ src/training_log.md
@@ -0,0 +1,4 @@
+##29/11/2019
+-> Started training at 17:40
+-> Environment shape = 550x550
+-> Nb iterations set = 100,000,000
